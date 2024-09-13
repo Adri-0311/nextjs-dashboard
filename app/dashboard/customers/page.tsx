475c96fdf86bcdf1ae1cbb66ff0516ed2a983162
Adri-0311 <77983983+Adri-0311@users.noreply.github.com>
2024-09-13 20:17:14 +0200

    Adding Metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: 'Customer',
+};
+
 export default function Page() {
-  return (
-    <p>Customer Page</p>
-  )
+	return <p>Customer Page</p>;
 }
